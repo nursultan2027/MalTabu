cc654b4661cd2543b337069369f6209a8c906dac
Nurs <nursultan.2027@gmail.com>
2018-11-05 16:53:41 +0600

    indicator

--- app/src/main/java/com/proj/changelang/activities/ShowDetails.java
+++ app/src/main/java/com/proj/changelang/activities/ShowDetails.java
@@ -1,5 +1,8 @@
 package com.proj.changelang.activities;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +10,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,27 +22,39 @@ import com.proj.changelang.models.ImageFragment;
 import com.proj.changelang.models.Post;
 
 public class ShowDetails extends AppCompatActivity {
-    private TextView title, content, price, phone, location, date;
+    private TextView title, content, price, phone, location, date, photos;
     private ImageView img;
     private Post post;
     private int PAGE_COUNT;
     private ViewPager pager;
     private PagerAdapter pagerAdapter;
+    private Dialog epicDialog;
+    private GestureDetector tapGestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_details);
+        epicDialog = new Dialog(this);
         post = getIntent().getParcelableExtra("post");
         initViews();
         setInfo();
         pager = (ViewPager) findViewById(R.id.pager);
         pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
-
+        tapGestureDetector = new GestureDetector(this, new TapGestureListener());
+        pager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                tapGestureDetector.onTouchEvent(event);
+                return false;
+            }
+        });
         pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
             @Override
             public void onPageSelected(int position) {
+                if(post.getImages().size()>0)
+                photos.setText(String.valueOf(position+1+"/"+post.getImages().size()));
             }
 
             @Override
@@ -57,6 +75,8 @@ public class ShowDetails extends AppCompatActivity {
         location.setText(post.getCityID());
         date.setText(post.getCreatedAt());
         PAGE_COUNT = post.getImages().size();
+        if(post.getImages().size()>0)
+            photos.setText(String.valueOf("1/"+post.getImages().size()));
     }
 
 
@@ -67,6 +87,7 @@ public class ShowDetails extends AppCompatActivity {
         phone = (TextView) findViewById(R.id.phoneTxt);
         location = (TextView) findViewById(R.id.locationTxt);
         date = (TextView) findViewById(R.id.dateTxt);
+        photos = (TextView) findViewById(R.id.photos);
     }
 
 
@@ -85,6 +106,23 @@ public class ShowDetails extends AppCompatActivity {
         public int getCount() {
             return PAGE_COUNT;
         }
+    }
 
+    class TapGestureListener extends GestureDetector.SimpleOnGestureListener{
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            sDialog();
+            return false;
+        }
+    }
+
+    protected void sDialog() {
+        epicDialog.setContentView(R.layout.image_dialog);
+        ViewPager asd = (ViewPager) epicDialog.findViewById(R.id.pages);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        asd.setAdapter(pagerAdapter);
+        epicDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        epicDialog.show();
     }
 }
